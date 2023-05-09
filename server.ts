9ee2d923416eda3127d6d05aacd665227eb7e9d1
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-05-09 09:37:20 +0200

    push

---
+++ server.ts
@@ -0,0 +1,12 @@
+//Steg 1
+//Importerar paket och skapar variabler för användning lite längre ned
+//Importerar express
+import express, {Request, Response, json} from 'express'
+//Använder variabeln app för att arbeta med express
+const app = express()
+//Bestämmer Port
+const port = 8000
+
+//Steg 2
+//Steg 3
+//Steg 4
